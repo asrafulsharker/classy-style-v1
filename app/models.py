57107e64babd3ebf598317554876d2145f3c0a94
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-06-16 12:15:45 +0600

    add some changes in models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator, MinLengthValidator,MinLengthValidator
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands', 'Andaman & NicobAr Islands'),
+    
+)
 
-# Create your models here.
+class Customer(nodels.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality =  models.CharField(max_length=200)
+    city =  models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+
+
+    def __str__(self):
+        return str(self.id)
+
+
+CATEGORY_CHOICES = (
+    ('M', 'Mobile'),
+    ('L', 'Laptop'),
+    ('TW', 'Top Wear'),
+    ('BW', 'Bottom Wear')
+)        
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length = 100)
+    category = models.CharField( choices=CATEGORY_CHOICES , max_length=2) 
