d223f9bdf3f9a38e899829fd3d1222725a4a525e
Md Asraful Sharker  Nirob <64266026+asrafulsharker@users.noreply.github.com>
2021-06-16 17:21:30 +0600

    add

--- app/models.py
+++ app/models.py
@@ -34,4 +34,16 @@ class Product(models.Model):
     brand = models.CharField(max_length = 100)
     category = models.CharField( choices=CATEGORY_CHOICES , max_length=2) 
     Product_image = models.ImageField(upload_to='productimg')
+
+    def __str__(self):
+        return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+        
+                 
     
